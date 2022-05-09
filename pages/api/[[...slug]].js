0bd49d3ed4ccc72d3ee70279370ddfb02e521161
hyperlook <hyperlook@gmail.com>
2022-05-09 20:43:34 +0800

    pathname 选择

--- pages/api/[[...slug]].js
+++ pages/api/[[...slug]].js
@@ -4,34 +4,40 @@ const CDN_SITE = 'https://google.glook.workers.dev/'
 
 
 export default async function handler(req, res) {
+
   let url = new URL(req.url, UP_SITE);
   let new_request_headers = new Headers(req.headers);
 
   new_request_headers.set('Host', url.hostname);
   new_request_headers.set('Referer', url.hostname);
 
-  try {
-    const original_response = await fetch(url.href, {
-      method: req.method,
-      headers: new_request_headers
-    })
-    const content_type = original_response.headers.get('content-type');
+  const pathnames = ['/', '/search','/complete/search']
 
-    //console.log(original_response.headers)
+  if (!pathnames.includes(url.pathname)) {
+    let sUrl = new URL(req.url, CDN_SITE);
+    res.redirect(sUrl.href);
+  } else {
+    try {
+      const original_response = await fetch(url.href, {
+        method: req.method,
+        headers: new_request_headers
+      })
+      const content_type = original_response.headers.get('content-type');
 
-    if (content_type.includes('text/html')) {
+      //console.log(original_response.headers)
+
+      console.log('当前路径为:' + url.pathname)
+      console.log('content-type:' + original_response.headers)
       let original_text = await original_response.text();
       res.setHeader('Content-Type', content_type);
       res.setHeader('access-control-allow-origin', '*');
       res.setHeader('access-control-allow-credentials', true);
       res.end(original_text);
-    } else {
-      let sUrl = new URL(req.url, CDN_SITE);
-      res.redirect(sUrl.href);
-    }
-  } catch (e) {
-    console.log(e)
-    console.log(url)    
-  }
 
+    } catch (e) {
+      console.log(e)
+      console.log(url)
+    }
+
+  }
 }
