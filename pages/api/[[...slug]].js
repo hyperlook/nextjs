cd07e3c19748f0d885a0d77c227641438dfb46a0
hyperlook <hyperlook@gmail.com>
2022-05-09 19:51:31 +0800

    删除大量无用代码

--- pages/api/[[...slug]].js
+++ pages/api/[[...slug]].js
@@ -1,28 +1,23 @@
-const up_website = 'https://www.google.com/'
+const UP_SITE = 'https://www.google.com/';
+const CDN_SITE = 'https://google.glook.workers.dev/'
 
 
 
 export default async function handler(req, res) {
-  let url = new URL(req.url, up_website);
-  let method = req.method;
-
-  let request_headers = req.headers;
-  let new_request_headers = new Headers(request_headers);
+  let url = new URL(req.url, UP_SITE);
+  let new_request_headers = new Headers(req.headers);
 
   new_request_headers.set('Host', url.hostname);
   new_request_headers.set('Referer', url.hostname);
 
-  // console.log(ua)
   try {
-
     const original_response = await fetch(url.href, {
-      method: method,
+      method: req.method,
       headers: new_request_headers
     })
-    let new_response_headers = new Headers(original_response.headers);
-    const content_type = new_response_headers.get('content-type');
+    const content_type = original_response.headers.get('content-type');
 
-    console.log(content_type)
+    //console.log(original_response.headers)
 
     if (content_type.includes('text/html')) {
       let original_text = await original_response.text();
@@ -31,16 +26,12 @@ export default async function handler(req, res) {
       res.setHeader('access-control-allow-credentials', true);
       res.send(original_text);
     } else {
-      let sUrl = new URL(req.url, 'https://google.glook.workers.dev/');
-      //console.log(sUrl.href)
+      let sUrl = new URL(req.url, CDN_SITE);
       res.redirect(sUrl.href);
-
     }
-
   } catch (e) {
     console.log(e)
-    //console.log(url)
-    console.log(url.href)
+    console.log(url)    
   }
 
 }
